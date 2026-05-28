605a16d8e7b24a499d0bae7931ef745f36fecfa8
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2026-05-28 17:20:54 -0400

    feat: technically working version
    didn't paste the original code entirely properly. Fixed it now.

--- src/sprintInput.cpp
+++ src/sprintInput.cpp
@@ -8,8 +8,11 @@ using namespace SKSE;
 using namespace SKSE::log;
 using namespace SKSE::stl;
 //taken from: https://github.com/ersh1/DodgeFramework/blob/master/src/Hooks.cpp
+//reworked as original implementation didn't seem to work without classic sprinting redone.
+//almost certainly imcompatible if you use skyparkour sprint key parkour. 
 
 namespace sprint {
+
     void Install() {
         log::info("Hooking sprintProcessEvent...");
 
@@ -23,12 +26,28 @@ namespace sprint {
     void SprintHandlerHook::ProcessButton(
         RE::SprintHandler* a_this, RE::ButtonEvent* a_event, RE::PlayerControlsData* a_data) {
 
-        if (settings::useSprint()) {
-            auto playerCharacter = RE::PlayerCharacter::GetSingleton();
-            auto userEvent = a_event->QUserEvent();
-            auto userEvents = RE::UserEvents::GetSingleton();
+        if (!a_this || !a_event || !a_data) {
+            return;
+        }
 
-            if (a_event->IsDown() && playerCharacter->playerFlags.isSprinting) {  // stopping sprint
+        if (!settings::useSprint()) {
+            return _ProcessButton(a_this, a_event, a_data);
+        }
+
+        auto* player = RE::PlayerCharacter::GetSingleton();
+        if (!player) {
+            return _ProcessButton(a_this, a_event, a_data);
+        }
+
+        auto* userEvents = RE::UserEvents::GetSingleton();
+        if (!userEvents) {
+            return _ProcessButton(a_this, a_event, a_data);
+        }
+
+        const auto userEvent = a_event->QUserEvent();
+
+        if (userEvent == userEvents->sprint) {
+            if (a_event->IsDown() && player->playerFlags.isSprinting) {
                 bStoppingSprint = true;
             } else if (a_event->HeldDuration() < settings::sprintDelay()) {
                 if (a_event->IsUp()) {
@@ -36,8 +55,8 @@ namespace sprint {
                     bStoppingSprint = false;
                 }
                 return;
-            } else if (playerCharacter && playerCharacter->playerFlags.isSprinting && !bStoppingSprint) {
-                a_event->heldDownSecs = 0.f;
+            } else if (!player->playerFlags.isSprinting && !bStoppingSprint) {
+                a_event->heldDownSecs = 0.0f;
             } else if (a_event->IsUp()) {
                 bStoppingSprint = false;
             }
