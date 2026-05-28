f51674eafe5b53459a48286bab4cce318d301037
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2026-05-28 19:56:33 -0400

    feat: somewhat working sprint dodge
    doesnt need classic sprinting redone but still a bit janky

--- src/sprintInput.cpp
+++ src/sprintInput.cpp
@@ -23,6 +23,16 @@ namespace sprint {
 
     static bool bStoppingSprint = false;
 
+    static bool IsPlayerSprinting(RE::PlayerCharacter* player) {
+        if (!player) {
+            return false;
+        }
+
+        bool isSprinting = false;
+        player->GetGraphVariableBool("IsSprinting", isSprinting);
+        return isSprinting;
+    }
+
     void SprintHandlerHook::ProcessButton(
         RE::SprintHandler* a_this, RE::ButtonEvent* a_event, RE::PlayerControlsData* a_data) {
 
@@ -44,22 +54,25 @@ namespace sprint {
             return _ProcessButton(a_this, a_event, a_data);
         }
 
-        const auto userEvent = a_event->QUserEvent();
+        /*const auto userEvent = a_event->QUserEvent();
 
         if (userEvent == userEvents->sprint) {
-            if (a_event->IsDown() && player->playerFlags.isSprinting) {
-                bStoppingSprint = true;
-            } else if (a_event->HeldDuration() < settings::sprintDelay()) {
-                if (a_event->IsUp()) {
-                    dodge::dodge();
-                    bStoppingSprint = false;
-                }
-                return;
-            } else if (!player->playerFlags.isSprinting && !bStoppingSprint) {
-                a_event->heldDownSecs = 0.0f;
-            } else if (a_event->IsUp()) {
+            return _ProcessButton(a_this, a_event, a_data);
+        }*/
+
+        const bool isSprinting = IsPlayerSprinting(player);
+        if (a_event->IsDown() && isSprinting) {
+            bStoppingSprint = true;
+        } else if (a_event->HeldDuration() < settings::sprintDelay()) {
+            if (a_event->IsUp()) {
+                dodge::dodge();
                 bStoppingSprint = false;
             }
+            return;
+        } else if (!isSprinting && !bStoppingSprint) {
+            a_event->heldDownSecs = 0.0f;
+        } else if (a_event->IsUp()) {
+            bStoppingSprint = false;
         }
 
         _ProcessButton(a_this, a_event, a_data);
